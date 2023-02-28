799f0b8d60b9db7487207e45ca3412db44b983e0
zakyuf <mochammadzakyz@gmail.com>
2023-02-28 16:16:14 +0700

    Praktikum 2 Langkah 2

--- 02_praktikum_web_lanjut_dua/routes/web.php
+++ 02_praktikum_web_lanjut_dua/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/articles/{id}', [PageController::class, 'article']);
