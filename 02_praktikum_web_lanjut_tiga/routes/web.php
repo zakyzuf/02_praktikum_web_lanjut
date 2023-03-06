258645b102e3df5297a4f1d760ffdeb13d606466
zakyuf <mochammadzakyz@gmail.com>
2023-03-06 20:37:56 +0700

    Praktikum 3

--- 02_praktikum_web_lanjut_tiga/routes/web.php
+++ 02_praktikum_web_lanjut_tiga/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('/product')->group(function (){
+    Route::get('/edugames', [ProductController::class, 'edugames']);
+    Route::get('/kidsgames', [ProductController::class, 'kidsgames']);
+    Route::get('/story', [ProductController::class, 'story']);
+    Route::get('/songs', [ProductController::class, 'songs']);
 });
+
+Route::get('/news/{$id}', [NewsController::class, 'news']);
+
+Route::prefix('/category')->group(function(){
+    Route::get('/karir', [CategoryController::class, 'karir']);
+    Route::get('/magang', [CategoryController::class, 'magang']);
+    Route::get('/kunjungan', [CategoryController::class, 'kunjungan']);
+});
+
+Route::get('/about', function(){
+    return redirect('https://www.educastudio.com/about-us');
+});
+
+Route::resource('/contact', ContactController::class)->only(['index']);
